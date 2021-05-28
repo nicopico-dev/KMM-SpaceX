844695758b9cad9ed7cdcda12c7a59369f617665
Nicolas Picon <nicolas.picon@in-tact.fr>
2021-05-28 15:24:46 +0200

    Add dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,17 @@
 buildscript {
+    val kotlinVersion = "1.5.10"
+    val sqlDelightVersion: String by project
+
     repositories {
         gradlePluginPortal()
         google()
         mavenCentral()
     }
     dependencies {
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.10")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
+        classpath("org.jetbrains.kotlin:kotlin-serialization:$kotlinVersion")
         classpath("com.android.tools.build:gradle:4.2.1")
+        classpath("com.squareup.sqldelight:gradle-plugin:$sqlDelightVersion")
     }
 }
 
